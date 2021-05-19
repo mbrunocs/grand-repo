ee30ad0dbf7534f6b81bb07c645f2f58e2093165
Marcio B Cyrilo <m.bruno.cs@hotmail.com>
2021-05-19 19:25:38 -0300

    4.1 JS Logica de Programaçao - Exercicio 2

---
+++ trybe-exercises/exercises/4.1/Exercicios/exer_II.js
@@ -0,0 +1,10 @@
+/* Exercício 2
+Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.*/
+let a = 1;
+let b = 2;
+
+if (a > b) {
+  console.log('O maior número é: '+a);
+} else {
+  console.log('O maior número é: '+b);
+}
