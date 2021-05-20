66b2806d04b00e7fd3f11b4461749675574ea80d
Marcio B Cyrilo <m.bruno.cs@hotmail.com>
2021-05-20 20:01:11 -0300

    Exercicios 4.2 - Exercicio 2

--- trybe-exercises/exercises/4.2/Exercicios/exer_2.js
+++ trybe-exercises/exercises/4.2/Exercicios/exer_2.js
@@ -1,2 +1,11 @@
 /*
 Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;*/
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let resultado = 0;
+
+for (let num of numbers){
+  resultado += num ;
+
+}
+  console.log(resultado);
