632510103728c2ff1be6afec2a4bc0c62a121df8
Marcio B Cyrilo <m.bruno.cs@hotmail.com>
2021-05-20 20:16:45 -0300

    Exercicios 4.2 - Exercicio 4

--- trybe-exercises/exercises/4.2/Exercicios/exer_4.js
+++ trybe-exercises/exercises/4.2/Exercicios/exer_4.js
@@ -1,3 +1,21 @@
 /*
 Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". 
 Caso não seja, imprima a mensagem: "valor menor ou igual a 20";*/
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+
+for (let num of numbers){
+  soma += num;
+}
+
+let media = soma / numbers.length;
+
+console.log(media);
+//this exercise
+
+if (media > 20){
+  console.log('Média maior que 20.');
+} else {
+  console.log('Média menor ou igual a 20');
+}
