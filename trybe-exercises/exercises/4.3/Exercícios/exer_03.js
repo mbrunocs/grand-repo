83eb450caf61d08e0076bcdb790bdf294c4d8663
Marcio B Cyrilo <m.bruno.cs@hotmail.com>
2021-05-21 20:32:00 -0300

    Logica de programaçao 4.3 - Exercicio 3 desvendando misterio

--- trybe-exercises/exercises/4.3/Exercícios/exer_03.js
+++ trybe-exercises/exercises/4.3/Exercícios/exer_03.js
@@ -10,14 +10,13 @@ n = 5
 let n = 6;
 let asteriscos = '';
 
-if (n > 1){
-  for (let cont = 0; cont < n; cont+= 1){
-    if (asteriscos === ''){
-      for (let index = 0; index < n; index += 1){
-        asteriscos += ' ';
-      }
+for (let cont = 0; cont < n; cont+= 1){
+  if (asteriscos === ''){
+    for (let index = 0; index < n; index += 1){
+      asteriscos += ' ';
+      console.log(asteriscos);
     }
-    asteriscos[n-1-cont] = '*';    
-    console.log(asteriscos);
   }
-} 
+  asteriscos[n-1-cont] = '*';    
+  console.log(asteriscos);
+}  
---
+++ trybe-exercises/exercises/4.3/Exercícios/exer_04.js
@@ -0,0 +1,11 @@
+/*
+Depois, faça uma pirâmide com n asteriscos de base:
+n = 5
+
+  *
+ ***
+*****/
+
+let n = 5;
+let base = '';
+
