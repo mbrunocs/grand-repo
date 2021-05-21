1afbcb1a512b43c144ca9bffc46cd81007b2cf9e
Marcio B Cyrilo <m.bruno.cs@hotmail.com>
2021-05-21 15:46:11 -0300

    Logica de programaçao 4.3 - Exercicio 1

---
+++ trybe-exercises/exercises/4.3/Exercícios/exer_01.js
@@ -0,0 +1,23 @@
+/* 
+Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , 
+imprima na tela um quadrado feito de asteriscos de lado de tamanho n . Por exemplo:
+n = 5;
+*****
+*****
+*****
+*****
+*****/
+
+let n = 0;
+let asteriscos;
+
+if (n > 1){
+  for (let cont = 0; cont <= n; cont+= 1){
+    asteriscos += '*';
+  }
+  for (let cont = 0; cont <= n; cont+= 1){
+    console.log(asteriscos);
+  }
+} else {
+  break;
+}
