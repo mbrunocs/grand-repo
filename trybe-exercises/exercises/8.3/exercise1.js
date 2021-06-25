25c4e4ec5628b571968869df31eb6ed873a164b0
Marcio B Cyrilo <m.bruno.cs@hotmail.com>
2021-06-25 18:37:54 -0300

    Resoluçao do exercicio 1

--- trybe-exercises/exercises/8.3/exercise1.js
+++ trybe-exercises/exercises/8.3/exercise1.js
@@ -63,4 +63,20 @@ const books = [
   },
 ];
 
-// Adicione o código do exercício aqui:
+// 1 - Crie um array com strings no formato NOME_DO_LIVRO - GÊNERO_DO_LIVRO - NOME_DA_PESSOA_AUTORA (Use a função map)
+
+const expectedResult = [
+  'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
+  'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
+  'Fundação - Ficção Científica - Isaac Asimov',
+  'Duna - Ficção Científica - Frank Herbert',
+  'A Coisa - Terror - Stephen King',
+  'O Chamado de Cthulhu - Terror - H. P. Lovecraft',
+];
+
+function formatedBookNames() {
+  const newArrayBooks = books.map((book) => `${book.name} - ${book.genre} - ${book.author.name}`);
+  return newArrayBooks;
+}
+
+assert.deepStrictEqual(formatedBookNames(), expectedResult);
