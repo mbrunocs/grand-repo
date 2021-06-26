1669852656f20dac9d9ac8c5eb3af45c1dd01fd0
Marcio B Cyrilo <m.bruno.cs@hotmail.com>
2021-06-25 23:25:59 -0300

    Aplicacao do exe 7, incompleto

--- trybe-exercises/exercises/8.2/exercise7.js
+++ trybe-exercises/exercises/8.2/exercise7.js
@@ -63,49 +63,13 @@ const books = [
   },
 ];
 
-/*
-2 - Construa um array de objetos a partir do array de livros. 
-  Cada objeto deve conter uma propriedade author , com o nome da pessoa autora do livro, e 
-  uma propriedade age com a idade dessa pessoa quando o livro foi lançado. 
-  O array deve ser ordenado por idade, ou seja, da pessoa mais jovem para a mais velha 
-  considerando suas idades quando o livro foi lançado.
-Dica: use as funções map , sort
-*/
-const expectedResult = [
-  {
-    age: 31,
-    author: 'Isaac Asimov',
-  },
-  {
-    age: 38,
-    author: 'H. P. Lovecraft',
-  },
-  {
-    age: 39,
-    author: 'Stephen King',
-  },
-  {
-    age: 43,
-    author: 'George R. R. Martin',
-  },
-  {
-    age: 45,
-    author: 'Frank Herbert',
-  },
-  {
-    age: 62,
-    author: 'J. R. R. Tolkien',
-  },
-];
+// 7 - Faça uma função que retorne true , caso nenhum author tenha nascido no mesmo ano, 
+//    e false , caso contrário.
 
-function nameAndAge() {
-  const list = books.map((book) => {
-    return {
-      age: book.releaseYear - book.author.birthYear,
-      author: book.author.name,
-    }
-  });
-  return books.sort((obj1, obj2) => obj1.age - obj2.age);
+const expectedResult = false;
+
+function authorUnique() {
+
 }
 
-assert.deepStrictEqual(nameAndAge(), expectedResult);
+assert.strictEqual(authorUnique(), expectedResult);
