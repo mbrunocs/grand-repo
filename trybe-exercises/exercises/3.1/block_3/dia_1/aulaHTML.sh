8a638655afca0ae54daa33663094b7cab9629b76
Marcio B Cyrilo <m.bruno.cs@hotmail.com>
2021-07-20 16:31:15 -0300

    Trocando o nome do diretorio para se adaptar melhor ao curso

--- trybe-exercises/exercises/3.1/block_3/dia_1/aulaHTML.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Meu primeiro script em Shell Script!"
-
-#Inicie seu código em HTML, para facilitar o início 
-# você pode digitar ! na primeira linha do script e 
-# irá aparecer a opção de autocomplete, se você 
-# apertar Enter a estrutura base de um arquivo HTML, 
-# com head parcialmente preenchido e com o body vazio, 
-# será inserida no arquivo.
--- trybe-exercises/exercises/3.1/block_3/dia_1/script.js
+++
@@ -1,2 +0,0 @@
-//Inicie aqui o seu codigo JavaScript
-//Execute o Code Runner com Ctrl + Alt + N
