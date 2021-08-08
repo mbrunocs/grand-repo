7c6ad050a54d6c9083e779b6846f4523e1cedcf0
Marcio B Cyrilo <m.bruno.cs@hotmail.com>
2021-08-08 15:30:30 -0300

    Exemplo 13.1 - API Rick e Morty

---
+++ Front-end/Intro-React/11.2/componentes/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import Image from './Image';
+
+function App() {
+  return (
+    <main>
+      <h1>My first React Class Component!</h1>
+      <Image src="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg" alt="Cute cat staring" />
+    </main>
+  );
+}
+
+export default App;
